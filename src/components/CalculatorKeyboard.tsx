24e9b07f018aa4d460bd2a0e73868ecce931c234
Yaroslav Ermilov <ya.ermilov@gmail.com>
2022-01-21 03:00:20 +0300

    Revert "Add experemental backdrop onclick to improve perfomance"
    This reverts commit b814e0bc716faa1cb73a03464362f22e9d48cad2.

--- src/components/CalculatorKeyboard.tsx
+++ src/components/CalculatorKeyboard.tsx
@@ -1,4 +1,4 @@
-import React, { MouseEventHandler, useCallback, useMemo, useRef } from 'react';
+import React from 'react';
 import { createUseStyles } from 'react-jss';
 import { ReactComponent as MultiplyIcon } from '../assets/icons/close.svg';
 import { ReactComponent as TrashIcon } from '../assets/icons/delete.svg';
@@ -29,41 +29,10 @@ const CalculatorKeyboard: React.FC<ICalculatorKeyboardProps> = () => {
         <CalculatorButton text={text} type={type} onPress={() => onKeyPress(text)} {...other} />
     );
 
-    const buttonsMap = useMemo(
-        () => [
-            ['C', '(', ')', '/'],
-            ['7', '8', '9', '*'],
-            ['4', '5', '6', '-'],
-            ['1', '2', '3', '+'],
-            ['0', '0', '.', '='],
-        ],
-        [],
-    );
-    const backdropRef = useRef<HTMLDivElement>(null);
-    const handleClick: MouseEventHandler<HTMLDivElement> = useCallback(
-        (e) => {
-            if (!backdropRef.current || e.target !== backdropRef.current) {
-                return;
-            }
-            const rect = backdropRef.current.getBoundingClientRect();
-            const x = e.pageX - rect.x;
-            const y = e.pageY - rect.y;
-            const col = Math.floor(x / (rect.width / 4));
-            const row = Math.floor(y / (rect.height / 5));
-            if (col < 0 || col > 3 || row < 0 || row > 4) {
-                return;
-            }
-            const button = buttonsMap[row][col];
-            onKeyPress(button);
-        },
-        [buttonsMap],
-    );
-
     const css = useStyle();
 
     return (
-        <div className={css.container} onClick={handleClick}>
-            <div ref={backdropRef} className={css.backdrop} />
+        <div className={css.container}>
             <Row>
                 <ActionButton text="C" icon={<TrashIcon />} onPress={() => onKeyPress('C')} />
                 <NumberButton text="(" type="action" />
@@ -102,13 +71,6 @@ export default CalculatorKeyboard;
 const useStyle = createUseStyles({
     container: {
         marginBottom: 20,
-        position: 'relative',
-    },
-    backdrop: {
-        position: 'absolute',
-        zIndex: 5,
-        width: '100%',
-        height: '100%',
     },
     row: {
         display: 'flex',
